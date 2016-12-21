8e5e72a8730cae50a844c634447a2bcd48cc57b2
unintendedbear <unintendedbear@gmail.com>
2016-12-21 13:26:12 +0100

    Plotting decision about their future studies depending on their gender

--- genderEngineering.r
+++ genderEngineering.r
@@ -11,3 +11,8 @@ responsesCFZV <- read.table(file = responsesCFZV_csv, header = TRUE, sep = ",")
 responsesBachLM_csv <- "/home/osica/Github/IEEE-Transactions-on-Education/Respuestas - La Madraza Bachillerato.csv"
 
 responsesBachLM <- read.table(file = responsesBachLM_csv, header = TRUE, sep = ",")
+
+theFuture <- factor(ifelse(responsesESOZV$Futuro == "Bachillerato de ciencias", "Yes", "No"))
+class(theFuture)
+plot(responsesESOZV$Mujer, theFuture)
+
